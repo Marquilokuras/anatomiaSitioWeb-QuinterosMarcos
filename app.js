3a11d638c596e3682cdfc2232893d389c24e2d3c
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-07-12 11:53:28 -0300

    HTML

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('El servidor está funcionando en el puerto '+PORT);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
