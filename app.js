ed181c3f081c614793bf1f84ef5287635aa20006
Marcos Quinteros <marquitos.q.27@gmail.com>
2023-08-02 11:29:14 -0300

    Arreglitos hosting

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log('El servidor está funcionando en el puerto '+PORT);
